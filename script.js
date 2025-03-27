1832be4c6534e62b7c104faa4a83bbd13c7fc548
drinnnird-uoft <drin.brown@alum.utoronto.ca>
2025-03-27 16:58:22 -0400

    major changes to script.js
    - add data layer for hexgrid
    - change opacity of hexgrid layer by mouseover
    - color hexgrid based on quantiles of transit time values
    - filter travel times by brand (populate brands from geoJSON file)

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const map = new mapboxgl.Map({
 
 const returnbutton = document.getElementById("returnbutton")
 
+let hoveredPolygonId = null; // set variable to hold ID of polygon hovered on, initialize to null
+
 returnbutton.addEventListener('click', (e) => {
     map.flyTo({
         center: [-79.41, 43.7],
@@ -15,6 +17,19 @@ returnbutton.addEventListener('click', (e) => {
         essential: true
     })
 })
+
+// function to find quantiles, for choropleth mapping
+const quantile = (arr, q) => {
+    const sorted = arr.sort();
+    const pos = (sorted.length - 1) * q;
+    const base = Math.floor(pos);
+    const rest = pos - base;
+    if (sorted[base + 1] !== undefined) {
+        return sorted[base] + rest * (sorted[base + 1] - sorted[base]);
+    } else {
+        return sorted[base];
+    }
+};
 
 map.on('load', () => {
 
@@ -59,6 +74,12 @@ map.on('load', () => {
         data: 'https://drinnnird-uoft.github.io/ggr472-project-foodaccess/data/supermarkets.geoJSON'
     })
 
+    map.addSource('sample-data', {
+        type: 'geojson',
+        data: 'https://drinnnird-uoft.github.io/ggr472-project-foodaccess/data/walmart_transit_sample.geoJSON',
+        'generateId': true //This ensures that all features have unique IDs 
+    })
+
     // draw features from geoJSON source files
 
     // draw boundary as grey lines
@@ -101,6 +122,36 @@ map.on('load', () => {
         closeOnClick: false
     });
 
+    map.on('mousemove', 'sample-poly', (e) => {
+        map.getCanvas().style.cursor = 'pointer'; // update the mouse cursor to a pointer to indicate clickability
+        if (e.features.length > 0) {
+            if (hoveredPolygonId !== null) {
+                map.setFeatureState(
+                    { source: 'sample-data', id: hoveredPolygonId },
+                    { hover: false }
+                );
+            }
+            hoveredPolygonId = e.features[0].id;
+            map.setFeatureState(
+                { source: 'sample-data', id: hoveredPolygonId },
+                { hover: true }
+            );
+        }
+    })
+
+    // When the mouse leaves the nh-poly layer, update the feature state of the
+    // previously hovered feature.
+    map.on('mouseleave', 'sample-poly', () => {
+        map.getCanvas().style.cursor = ''; // put the mouse cursor back to default
+        if (hoveredPolygonId !== null) {
+            map.setFeatureState(
+                { source: 'sample-data',id: hoveredPolygonId },
+                { hover: false }
+            );
+        }
+        hoveredPolygonId = null;
+    });
+
     // note: Mapbox has a known issue with mouseenter and mouseleave for circle layers
     // the points are right-biased meaning you have to mouse over the right edge of the circle
     // rather than the center
@@ -134,6 +185,92 @@ map.on('load', () => {
         map.getCanvas().style.cursor = '';
         popup.remove();
     });
+
+    // go through travel time data to build color ramp
+
+    let samplejson;
+
+    fetch('https://drinnnird-uoft.github.io/ggr472-project-foodaccess/data/walmart_transit_sample.geoJSON')
+    .then(response => response.json())
+    .then(response => {
+        samplejson = response;
+        console.log(samplejson);
+
+        // get quartiles for choropleth scaling
+        let ttimes = [];
+
+        samplejson.features.forEach((label, i) => {
+            let feature = samplejson.features[i];
+            let props = feature.properties;
+            let travtime = props.travel_time;
+            ttimes.push(travtime) 
+        })
+
+        const q1 = quantile(ttimes, 0.25);
+        const med = quantile(ttimes, 0.5);
+        const q3 = quantile(ttimes, 0.75);
+        const upper = Math.max.apply(null, ttimes);
+
+        map.addLayer({
+            'id' : 'sample-poly',
+            'type' : 'fill',
+            'source' : 'sample-data',
+            'paint': {
+                "fill-color" : [
+                    "step",
+                    ["get", "travel_time"],
+                    "#fef0d9",
+                    q1, "#fdcc8a",
+                    med, "#fc8d59",
+                    q3, "#e34a33",
+                    upper, "#b30000"
+                ],
+                'fill-opacity': [ // set the fill opacity based on a feature state which is set by a hover event listener
+                'case',
+                ['boolean', ['feature-state', 'hover'], false],
+                1,  // opaque when hovered on
+                0.5 // semi-transparent when not hovered on
+            ],
+            'fill-outline-color': 'white'
+            },
+             'filter': ['all', ['has', 'travel_time'], ['!=', ['get', 'travel_time'], null]] // show only hexgrid points that have a travel time set
+        })
+
+        // hide the hexgrid at first until something is selected in the dropdown
+        map.setLayoutProperty("sample-poly", 'visibility', 'none');
+
+        // handle clicking on a hexgrid item
+        map.on('click', 'sample-poly', (e) => {
+            const coordinates = e.features[0].geometry.coordinates.slice();
+            const travel_time = e.features[0].properties.travel_time;
+
+            const sel_travel_mode = $(".iconfilter-clicked");
+            if(sel_travel_mode.length == 1) {
+                const pieces = sel_travel_mode[0].id.split("btn");
+                $("#click-info").html("Travel time to " + $("#chain-select").val() + " by " + pieces[1] + " is " + travel_time + "m.");
+            }
+        })
+    })
+
+    // click handler for selecting a brand inside map load because the map must be loaded to apply filters
+    $("#chain-select").change(function() {
+        let sel = $(this).val();
+        console.log("Selected " + sel)
+
+        if(sel !== 0) {
+            // if something has been selected
+            // show the hexgrid for that selection
+            
+            map.setLayoutProperty("sample-poly", "visibility", "visible");
+            map.setFilter('sample-poly', ['all', 
+                ['has', 'travel_time'],
+                ['!=', ['get', 'travel_time'], null],
+                ['==', ['get', 'brand'], sel]
+            ]);
+
+            map.setFilter('super-point', ['==', ['get','brand'], sel]); // show only supermarkets that match requested brand
+        }
+    })
 
 })
 
@@ -185,9 +322,6 @@ $(document).ready(function() {
         $("#btnTransit").removeClass("iconfilter-clicked")
         $("#btnWalk").removeClass("iconfilter-clicked")
     })
-    $("#chain-select").click(function() {
-        console.log("Selected " + $(this).val())
-    })
 
 
     // inspect the properties of the geoJSON file
@@ -211,6 +345,8 @@ $(document).ready(function() {
                 }
             }
         });
+
+        brands.push("Walmart"); // debugging, remove this later
 
         brands.sort();
 
